51938f8cb9ac031e27740cfc44271bb8fa8dc9f4
Nasir-os <Abdinasir_Farah@outlook.com>
2023-01-27 02:35:54 -0700

    move all id key value to bottom

--- script.js
+++ script.js
@@ -1,35 +1,53 @@
-import {menuArray} from "./data.js";
+import { menuArray } from "./data.js";
 
-const main =  document.querySelector('main')
-let menuHtml = ''
+document.addEventListener("click", (e) => {
+  if (e.target.id) {
+    handleAddBtnClicks(e.target.id);
+  }
+});
+let orderedItemHtml;
+function handleAddBtnClicks(itemId) {
+  const targetOrderItem = menuArray.filter(function (item) {
+    return parseInt(itemId) === item.id;
+  })[0];
+  orderedItemHtml = `
+  <div class="ordered-item">
+      <h5 class="item-title">${targetOrderItem.name}</h5>
+      <button class="remove-btn">remove</button>
+      <h5 class="item-price">$${targetOrderItem.price}</h5>
+  </div>
 
-function renderMenuArray(){
-    for(let item of menuArray){
-       menuHtml +=   
-      `
-      <div class="item-container" id="${item.id}">
-            <div class="item-graphic">${item.emoji}</div>
-            <div class="item-details">
-              <h3 class="item-title">${item.name}</h3>
-              <p class="item-description">${item.ingredients}</p>
-              <h5 class="item-price">$${item.price}</h5>
-            </div>
-            <button class="add-btn">+</button>
-          </div>
-      
-      ` 
-    }
-    main.innerHTML = menuHtml
 
-   
+  `;
+  const checkOutSection = document.getElementById('checkout-section')
+  checkOutSection.style.opacity ='1'
+  const checkoutOrders = document.querySelector(".checkout-orders");
+  checkoutOrders.innerHTML += orderedItemHtml;
 }
 
-console.log(document.querySelectorAll('.add-btn'));
+function getMenuHtml() {
+  let menuHtml = "";
 
+  for (let item of menuArray) {
+    menuHtml += `
+        <div class="item-container">
+              <div class="item-graphic">${item.emoji}</div>
+              <div class="item-details">
+                <h3 class="item-title">${item.name}</h3>
+                <p class="item-description">${item.ingredients}</p>
+                <h5 class="item-price">$${item.price}</h5>
+              </div>
+              <button class="add-btn" id="${item.id}">+</button>
+            </div> 
+        
+        `;
+  }
+  
+  return menuHtml;
+}
 
+function render() {
+  document.querySelector("main").innerHTML = getMenuHtml();
+}
 
-
-
-
-
-renderMenuArray()
+render();
